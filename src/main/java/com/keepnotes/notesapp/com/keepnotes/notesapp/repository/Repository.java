884764b635736b033160c3a5189072779bf7eec8
JayanDesilva <jaycdesilva@gmail.com>
2024-07-28 16:25:37 -0400

    findAll() method added

---
+++ src/main/java/com/keepnotes/notesapp/com/keepnotes/notesapp/repository/Repository.java
@@ -0,0 +1,6 @@
+package com.keepnotes.notesapp.com.keepnotes.notesapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Repository extends JpaRepository {
+}
